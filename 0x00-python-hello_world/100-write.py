6ab68398fa56062886c629c1b8d138b736026b05
sgsod <gamanorman3@gmail.com>
2024-03-12 23:43:33 +0200

    pycodestyle fix 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,9 +2,9 @@
 import sys
 
 def main():
-	error_message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-	sys.stderr.write(error_message + "\n")
-	sys.exit(1)
+    error_message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(error_message + "\n")
+    sys.exit(1)
 
 if __name__ == "__main__":
-        main()
+    main()
