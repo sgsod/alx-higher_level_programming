3445c682176cd5f3f6187a2c73a5218576d78220
sgsod <gamanorman3@gmail.com>
2024-03-12 05:19:40 +0200

    100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+	error_message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+	sys.stderr.write(error_message + "\n")
+	sys.exit(1)
+
+if __name__ == "__main__":
+        main()
