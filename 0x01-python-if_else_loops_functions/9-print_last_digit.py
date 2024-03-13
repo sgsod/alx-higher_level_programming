cb5718ff034842182c5db23bc689d438877c868e
sgsod <gamanorman3@gmail.com>
2024-03-13 04:28:19 +0200

    add 10-add.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-# calculate the last digit and store in last_digit
+    # calculate the last digit and store in last_digit
     if number >= 0:
         last_digit = number % 10
     else:
-        last_digit = number % -10
-# print last digit
+        last_digit = (number % -10) * -1
+    # print last digit
     print("{}".format(last_digit), end="")
     return last_digit
