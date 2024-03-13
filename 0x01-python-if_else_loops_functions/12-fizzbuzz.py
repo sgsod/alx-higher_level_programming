30b13ac9cd94ead013eb282910415d33898cd247
sgsod <gamanorman3@gmail.com>
2024-03-13 05:08:00 +0200

    fix#1: 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(101):
-        for j in range(i, i + 1)
+        for j in range(i, i + 1):
             if i % 3 == 0 or i % 5 == 0:
                 if i % 3 == 0:
                     print("Fizz", end="")
