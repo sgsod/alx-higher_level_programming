64e671b0ee94d4a117474fd40c4978fa954c86d1
sgsod <gamanorman3@gmail.com>
2024-03-13 03:08:33 +0200

    add 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(99):
-    print("{:0^2}".format(i), end=", ")
-print("99")
+    print("{:0>2}".format(i), end=", ")
+# 99 is not accessed in for loop
+print("{}".format(i + 1))
