ba126ba58c35add63598b3616a30374fc05f7a12
sgsod <gamanorman3@gmail.com>
2024-03-18 16:38:00 +0200

    Python data structures

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+print_list_integer: function that prints integers from input list
+print one element per line
+'''
+def print_list_integer(my_list=[]):
+    # iterate through my_list
+    for elem in my_list:
+        print("{}".format(elem))
