9ce9776581b2949cb19af484f6da36d30bce4d7a
sgsod <gamanorman3@gmail.com>
2024-03-18 16:43:15 +0200

    add 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,8 @@
 print_list_integer: function that prints integers from input list
 print one element per line
 '''
+
+
 def print_list_integer(my_list=[]):
     # iterate through my_list
     for elem in my_list:
