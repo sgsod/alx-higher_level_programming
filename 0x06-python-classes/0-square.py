995039baa6485fbca2e39eb8b160ba89ee982634
sgsod <gamanorman3@gmail.com>
2024-04-08 17:56:14 +0200

    python class trial

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Containing an empty class.
+
+Sqaure simply usesthe placeholder pass.
+"""
+
+class Square:
+    """Should calculate squares."""
+    def __init__(self):
+    """Initializes the instance."""
+        pass
