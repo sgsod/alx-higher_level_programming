d7722b83b8460528f548a2024ddb666a3be6ea30
sgsod <gamanorman3@gmail.com>
2024-04-17 03:34:50 +0200

    0-read_file.py fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filenamme=""):
     """read and pint file"""
 
     with open(filename, encoding="utf-8") as s:
-        print("{:s}".format(filename.read()))
+        print("{:s}".format(s.read()))
