f6c439e7ddc0ca3b207b273faf5450f447e01279
sgsod <gamanorman3@gmail.com>
2024-05-23 11:57:55 +0200

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates MySQL server user (user_0d_1).
+-- grant user all privileges of current database
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES
